0aa930e34407f546e7550215d27e7b3160ae96ad
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2023-07-18 14:36:25 -0400

    Generic Button
    Adding a generic button with an action listener for easy button creation

---
+++ MyButton.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyButton extends JButton
+{
+    public MyButton(String v, TextField display)
+    {
+        Font buttonFont = new Font("Sans Serif", Font.PLAIN, 20);
+        this.setFont(buttonFont);
+        this.setName(v);
+        this.setText(v);
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String m1 = display.getText();
+                m1 += v;
+                display.setText(m1);
+                display.validate();
+                display.repaint();
+            }
+        });
+    }
+}
